777f3091adcbf9957ba8684d61b72e76828c0b48
victor110298 <44604893+victor110298@users.noreply.github.com>
2019-03-19 21:16:05 +0200

    Create ForkJoinPoolEx class1

--- src/executors/ForkJoinPoolEx.java
+++ src/executors/ForkJoinPoolEx.java
@@ -7,18 +7,19 @@ import static executors.ForkJoinPoolEx.countOfOperation;
 import static executors.ForkJoinPoolEx.numberOfThread;
 
 public class ForkJoinPoolEx {
-    public static int numberOfThread=Runtime.getRuntime().availableProcessors();
+    public static int numberOfThread = Runtime.getRuntime().availableProcessors();
     private static ForkJoinPool fjp = new ForkJoinPool(numberOfThread);
     public static long countOfOperation = 1000L;
 
     public static void main(String[] args) {
-        System.out.println("Count of thread "+numberOfThread);
+        System.out.println("Count of thread " + numberOfThread);
         System.out.println("Start!");
-        System.out.println(fjp.invoke(new Constructor(0,countOfOperation)));
+        System.out.println(fjp.invoke(new Constructor(0, countOfOperation)));
         System.out.println("End!");
     }
 }
-class Constructor extends RecursiveTask<Long>{
+
+class Constructor extends RecursiveTask<Long> {
     private long from;
     private long to;
 
@@ -27,23 +28,21 @@ class Constructor extends RecursiveTask<Long>{
         this.to = to;
     }
 
-
     @Override
     protected Long compute() {
-        if((to-from) <= (countOfOperation/numberOfThread)){
-            long result=0;
-            for (long i=from; i<to; i++){
-                result+=i;
+        if ((to - from) <= (countOfOperation / numberOfThread)) {
+            long result = 0;
+            for (long i = from; i < to; i++) {
+                result += i;
             }
             return result;
-        }else {
-            long middle=(to+from)/2;
-            Constructor firstHalf=new Constructor(from,middle);
+        } else {
+            long middle = (to + from) / 2;
+            Constructor firstHalf = new Constructor(from, middle);
             firstHalf.fork();
-            Constructor secondtHalf=new Constructor(middle+1, to);
-            long secondValue= secondtHalf.compute();
-            return firstHalf. join()+secondValue;
-
+            Constructor secondtHalf = new Constructor(middle + 1, to);
+            long secondValue = secondtHalf.compute();
+            return firstHalf.join() + secondValue;
         }
     }
 }
